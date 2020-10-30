cd836795e91e6a79a10caabdcac748d9531149f3
Breeze-Sj <52182873+Breeze-Sj@users.noreply.github.com>
2020-10-29 21:26:37 -0600

    updated files, added 2nd js file

--- Develop/script.js
+++ Develop/script.js
@@ -13,9 +13,24 @@ var spotifyAuthLink =
 //token is spotify giving you authorization to use that account
 var token;
 //on click of authentication button set the url (window.location.href = spotifyAuthLink) to the spotify auth link
+
+$("#auth").on('click', function(){
+  (window.location.href = spotifyAuthLink);
+  token = location.hash.substr(1);
+  var bear = parseInt("Bearer "+token);
+  console.log(bear);
+ 
+
+});
+
 //click listener on button in index.html
 //get authorization token from url and add 'Bearer' to the beginning of the string (location.hash.substr(1)) <- how you get url
 //after user logs in, store that token as a variable, parse from url, then do "Bearer "+token
+
+
+$("main-search").on('click', function(){
+
+})
 //on click of search button run a search function that includes the fetch call
 //add token variable in as header, google how to add header to a fetch
 //make the fetch call with the authorizationToken as a header
