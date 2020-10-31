737fe5e08a0f3ed07aa16fcc506ea90e02625c37
Breeze-Sj <52182873+Breeze-Sj@users.noreply.github.com>
2020-10-31 11:12:23 -0600

    created new Variables

--- Develop/script2.js
+++ Develop/script2.js
@@ -1,11 +1,14 @@
 //click listener on button in index.html
 //get authorization token from url and add 'Bearer' to the beginning of the string (location.hash.substr(1)) <- how you get url
 //after user logs in, store that token as a variable, parse from url, then do "Bearer "+token
-console.log(location.href)
-var token = "BQAoP6JXXFoqm2_mUh8umZ1j3nFJV1yVMPgPBgLPnWYWHvHs6baNoBZeeEDTW90HGg_r42MKJxGZR8KSnGjnNtZeBse7dbn_e4g6amsGROGJCHOSB8tKU89HK_X8VeKdp7IUziPbjAvop-MkxJqTmnmG&token_type=Bearer"
 
-$("main-search").on('click', function(){
+var equal = location.href.indexOf("=")
+var amp = location.href.indexOf("&")
+var token = location.href.substr(equal +1, amp)
+console.log(token)
 
+$("main-search").on('click', function(event){
+  event.preventDefault()
 })
 
 var apiUrl = "https://nickkeller316.github.io/Project1/Develop/index2.html#access_token=" + token +"&token_type=Bearer&expires_in=3600"
