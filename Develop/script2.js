0cea936c66fda280f53fbe43fb7df76a47a23ee9
Breeze-Sj <52182873+Breeze-Sj@users.noreply.github.com>
2020-10-31 10:50:51 -0600

    Continued fetch API in JS

--- Develop/script2.js
+++ Develop/script2.js
@@ -7,12 +7,26 @@ var token;
 $("main-search").on('click', function(){
 
 })
-fetch('URL_GOES_HERE', { 
-    method: 'get', 
+
+var apiUrl = "https://nickkeller316.github.io/Project1/Develop/index2.html#access_token=BQAoP6JXXFoqm2_mUh8umZ1j3nFJV1yVMPgPBgLPnWYWHvHs6baNoBZeeEDTW90HGg_r42MKJxGZR8KSnGjnNtZeBse7dbn_e4g6amsGROGJCHOSB8tKU89HK_X8VeKdp7IUziPbjAvop-MkxJqTmnmG&token_type=Bearer&expires_in=3600"
+
+
+
+fetch(apiURL)   
+  .then(function(response){
+    return response.json();
+  })
+   .then(function(data){
+     console.log(data)
+   })
+
+  
     headers: new Headers({
       'Authorization': 'Bearer ' + token
     })
-  })
+  
 //on click of search button run a search function that includes the fetch call
 //add token variable in as header, google how to add header to a fetch
 //make the fetch call with the authorizationToken as a header
+
+
